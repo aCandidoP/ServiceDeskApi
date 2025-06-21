1e741b433c0eec3e485351cac40a93fa5ee84547
acpereira <andrepereira2611@gmail.com>
2025-06-21 13:50:57 -0300

    ajustando blue prin

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,9 +4,10 @@ usuario_bp = Blueprint('usuario', __name__, url_prefix='/usuarios')
 chamado_bp = Blueprint('chamado', __name__, url_prefix='/chamados')
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 categoria_bp = Blueprint('categoria', __name__, url_prefix='/categorias')
-
+tipo_bp = Blueprint('tipo', __name__, url_prefix='/tipos')
 
 from app.routes.usuario_routes import *
 from app.routes.chamado_routes import *
 from app.routes.auth_routes import *
-from app.routes.categorias_routes import *
+from app.routes.categoria_routes import *
+from app.routes.tipo_routes import *
